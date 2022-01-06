b1a9f299596cd1549a67ecbd7923c65368fb77fc
Brayan Matias <omatias@lumston.com>
2022-01-05 18:34:04 -0600

    Avances

---
+++ src/app/pages/notpagefound/notpagefound.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-notpagefound',
+  templateUrl: './notpagefound.component.html',
+  styleUrls: ['./notpagefound.component.css'],
+})
+export class NotpagefoundComponent {
+  year = new Date().getFullYear();
+}
